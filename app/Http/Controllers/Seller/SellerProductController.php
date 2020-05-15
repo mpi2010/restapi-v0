65a6b6947dacfe28daf29e589fe8cc0d236c09f9
mpi2010 <mpi@mpi.biz.ua>
2020-05-16 00:51:42 +0300

    Modul 15

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -11,7 +11,7 @@ class SellerProductController extends ApiController
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index(Seller $seller)
     {
